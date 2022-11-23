6886d3854a43a6c0448dc2492e17c1de35a9d169
Mikael Trilles <11914687-mikaeltrilles@users.noreply.gitlab.com>
2022-11-23 16:25:58 +0100

    🚀 RELEASE: TP8

--- tp08-addition.js
+++ tp08-addition.js
@@ -1,6 +1,16 @@
+// Fonction addition de la page tp08-addition.html
+
+// Déclaration des variables
+var nb1, nb2, resultat;
+
+// Fonction addition
 function addition() {
-  var a = document.getElementById("a").value;
-  var b = document.getElementById("b").value;
-  var resultat = parseInt(a) + parseInt(b);
-  document.getElementById("resultat").innerHTML = resultat;
+  nb1 = parseInt(document.getElementById("a").value);
+  nb2 = parseInt(document.getElementById("b").value);
+  if (isNaN(nb1) || isNaN(nb2)) {
+    document.getElementById("resultat").innerHTML = "Veuillez saisir des nombres";
+  } else {
+    resultat = nb1 + nb2;
+    document.getElementById("resultat").innerHTML = resultat;
+  }
 }
